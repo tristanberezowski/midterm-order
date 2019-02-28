2226d1611a3514b423bea17927952d8461837425
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-28 21:44:23 +0000

    Adds JQuery data reference app.js to render products for index

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,48 @@
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });;
-// });
-
 $(document).ready(function() {
-  const menu = {
-    item1: {
-      id: "100",
-      name: "Hot Dog",
-      description: "gourmet sandwich",
-      price: "$3,99"
-    },
-    item2: {
-      id: "200",
-      name: "Hamburguer",
-      description: "Double Cheese",
-      price: "$7,99"
-    }
+  const createProductMenu = function(product) {
+    //returns jquery object
+    let menuHtml = `
+    <article class="order-1 product1">
+    <div class="card">
+      <img
+        class="card-img-top"
+        src="${product.image}"
+        alt=""
+      />
+      <div class="card-body">
+        <div class="row">
+          <div class="col-12 col-md-8"><h5 class="card-title">${product.name}</h5></div>
+          <div class="col-12 col-md-4"><h6 class="product-price">${product.price}</h6></div>
+        </div>
+        <p class="card-text">${product.description}</p>
+        <form class="product-form" method="POST" action="/">
+          <div class="quantity-btn" id="decrease" onclick="decreaseValue()" value="Decrease Value">
+            <span>-</span>
+          </div>
+          <input type="number" id="quantity" value="0" />
+          <div class="quantity-btn" id="increase" onclick="increaseValue()" value="Increase Value">
+            <span>+</span>
+          </div>
+        </br>
+      </br>
+          <button href="#" class="add-to-cart-btn">Add to Cart</button>
+        </form>
+      </div>
+    </div>
+  </article>
+  `;
+    let $menuHtml = $(menuHtml);
+    return $menuHtml;
   };
 
-  function createOrderItem(menu) {}
-}); //closes jquery function
+  $(() => {
+    $.ajax({
+      method: "GET",
+      url: "/api/products"
+    }).done(products => {
+      for (product of products) {
+        createProductMenu(product).appendTo("#product-container");
+      }
+    });
+  });
+}); //closes JQuery function
