1062d07475c44102eab38d0b1e9475c0a1a07360
vnctwong <vincentkywong@gmail.com>
2019-02-28 01:28:08 +0000

    adds 5 tables to database, migrates properly

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,11 @@
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
+  console.log(process.env);
   return knex.schema.createTable('users', function (table) {
     table.increments();
     table.string('name');
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTable('users');
 };
