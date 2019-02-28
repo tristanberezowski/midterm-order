8cdd2615ae18b3bb4b42628fbde466850c860df5
Tristan <tristanberezowski@gmail.com>
2019-02-28 20:31:08 +0000

    changed seeding for image urls to work

--- db/seeds/1seed_dummy.js
+++ db/seeds/1seed_dummy.js
@@ -12,21 +12,21 @@ exports.seed = function (knex, Promise) {
           name: 'hot dog',
           description: 'costco',
           price: 3.33,
-          img: '../images/flag.png'
+          img: './images/flag.png'
         }),
         knex('products').insert({
 
           name: 'burger',
           description: 'mcd',
           price: 4.66,
-          img: '../images/heart.png'
+          img: './images/heart.png'
         }),
         knex('products').insert({
 
           name: 'pizza',
           description: 'hut',
           price: 7.55,
-          img: '../images/refresh.png'
+          img: './images/refresh.png'
         })
       ]);
     });
