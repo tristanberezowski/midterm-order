820fb5bf9eac2e5edf416c3c097aeabe3d8e6c7a
Tristan <tristanberezowski@gmail.com>
2019-03-01 02:50:30 +0000

    more dummy data for seeds in single-order, testing routes

--- db/seeds/1seed_dummy.js
+++ db/seeds/1seed_dummy.js
@@ -8,21 +8,21 @@ exports.seed = function (knex, Promise) {
         // DO NOT MAKE CHANGES TO FOREIGN KEYS
 
         knex('products').insert({
-
+          id: 1,
           name: 'hot dog',
           description: 'costco',
           price: 3.33,
           img: './images/flag.png'
         }),
         knex('products').insert({
-
+          id: 2,
           name: 'burger',
           description: 'mcd',
           price: 4.66,
           img: './images/heart.png'
         }),
         knex('products').insert({
-
+          id: 3,
           name: 'pizza',
           description: 'hut',
           price: 7.55,
