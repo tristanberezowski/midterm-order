c2613559dc411dca9abad6816663c816be5a073e
Tristan <tristanberezowski@gmail.com>
2019-03-01 23:26:44 +0000

    remove from sidebar works properly with cart implementation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,82 +1,105 @@
-$(document).ready(function() {
-  // Check if quantity is at least 1
-  const checkQuantity = function(cartItem, $quantityInput) {
-    if (Number($quantityInput.val()) < 1) {
-      alert("Please add at least 1 item.");
-    } else {
-      addToCart(cartItem);
-      cartTotal();
-    }
-  };
+function infoForCart($element) {
+const name = $(".card-title", $element).text();
+const price = $(".product-price", $element).text();
+const quantity = $(".quantity-input", $element).val();
+const totalItem = (quantity * price).toFixed(2);
+return { name: name, price: price, quantity: quantity, total: totalItem };
+}
+
+const checkQuantity = function(cartItem, $quantityInput) {
+// Check if quantity is at least 1
+if (Number($quantityInput.val()) < 1) {
+  alert("Please add at least 1 item.");
+} else {
+  addToCart(cartItem);
+  cartTotal();
+}
+};
+
+// Creates restaurant menu
+const createProductMenu = function(product) {
+//returns jquery object
+let menuHtml = `
+<article class="p-2">
+<div class="card">
+  <img class="card-img-top" src="${product.img}"/>
+  <div class="card-body">
+    <div class="row">
+      <div class="col-12 col-md-8"><h5 class="card-title">${product.name}</h5></div>
+      <div class="col-12 col-md-4"><h6 class="product-price">${product.price}</h6></div>
+    </div>
+    <p class="card-text">
+      ${product.description}
+    </p>
+    <div class="decrease quantity-btn" onclick="decreaseValue()" value="Decrease Value">
+      <span>-</span>
+    </div>
+    <input type="number" class="quantity-input" value="0" min="1" step="1" />
+    <div class="increase quantity-btn" onclick="increaseValue()" value="Increase Value">
+      <span>+</span>
+    </div>
+    </br>
+    </br>
+    <button class="add-to-cart-btn">Add to Cart</button>
+  </div>
+</div>
+</article>
+`;
+let $menuHtml = $(menuHtml);
+return $menuHtml;
+};
+
+function cartTotal() {
+
+}
+
+// Creates html structure in the cart
+function addToCart(item) {
+let cartItem = `
+  <article class="cart-item">
+    <div class="row">
+        <div class="col-12 col-md-2 quantity"><b>${item.quantity}</b></div>
+        <div class="col-12 col-md-6 item-name"><b>${item.name}</b></div>
+        <div class="col-12 col-md-4 item-price">$ ${item.total}</div>
+    </div>
+    <div class="remove-btn">
+      <a href="#">Remove</a>
+    </div>
+  </article>`;
+return $("#side-bar").append(cartItem);
+}
+
+$(() => {
+  
+
+  var cart = [];
+
   // Event listener to add items to cart
   $("#product-container").on("click", ".add-to-cart-btn", function() {
     const $parent = $(this).parent();
     const cartItem = infoForCart($parent);
+    cart.push(cartItem);
     checkQuantity(cartItem, $parent.children(".quantity-input"));
   });
 
   // Event listener to remove items from cart
   $("#side-bar").on("click", ".remove-btn", function() {
+    console.log(cart);
     const $parent = $(this).parent();
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].name === $parent.find(".item-name").text())
+        cart.splice(i, 1);
+    }
+    console.log(cart);
     $parent.remove();
   });
-  // Checks if cart is empty or not
-  function cartTotal() {
-    if ($("#side-bar").has(".cart-item").length) {
-      console.log("Cart has Item");
-    } else {
-      console.log("Cart is Empty");
-    }
-  }
   cartTotal();
-  // Creates restaurant menu
-  const createProductMenu = function(product) {
-    //returns jquery object
-    let menuHtml = `
-    <article class="p-2">
-    <div class="card">
-      <img class="card-img-top" src="${product.img}"/>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-12 col-md-8"><h5 class="card-title">${product.name}</h5></div>
-          <div class="col-12 col-md-4"><h6 class="product-price">${product.price}</h6></div>
-        </div>
-        <p class="card-text">
-          ${product.description}
-        </p>
-        <div class="decrease quantity-btn" onclick="decreaseValue()" value="Decrease Value">
-          <span>-</span>
-        </div>
-        <input type="number" class="quantity-input" value="0" min="1" step="1" />
-        <div class="increase quantity-btn" onclick="increaseValue()" value="Increase Value">
-          <span>+</span>
-        </div>
-        </br>
-        </br>
-        <button class="add-to-cart-btn">Add to Cart</button>
-      </div>
-    </div>
-  </article>
-  `;
-    let $menuHtml = $(menuHtml);
-    return $menuHtml;
-  };
+  
+  
 
-  // Creates html structure in the cart
-  function addToCart(item) {
-    let cartItem = `
-      <article class="cart-item">
-        <div class="row">
-            <div class="col-12 col-md-2 quantity"><b>${item.quantity}</b></div>
-            <div class="col-12 col-md-6 item-name"><b>${item.name}</b></div>
-            <div class="col-12 col-md-4 item-price">$ ${item.total}</div>
-        </div>
-        <div class="remove-btn">
-          <a href="#">Remove</a>
-        </div>
-      </article>`;
-    return $("#side-bar").append(cartItem);
-  }
+  
+
+  
 
   // Ajax request to create products menu
   $(() => {
