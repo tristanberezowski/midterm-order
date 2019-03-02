83d4efea2c78c3ecec8b94800382ddd22e1617f9
vnctwong <vincentkywong@gmail.com>
2019-03-02 22:05:54 +0000

    adds restaurant_button.js script

---
+++ public/scripts/restaurant_button.js
@@ -0,0 +1,17 @@
+//CHECK ALL VARIBLES, QUERIES, AND ROUTE NAMES
+//CHECK ALL VARIBLES, QUERIES, AND ROUTE NAMES
+//CHECK ALL VARIBLES, QUERIES, AND ROUTE NAMES
+$(() => {
+
+  $('confirm-order-button-ID').on('submit', (event) => {
+    event.preventDefault();
+
+
+    $.post('/restaurant')
+
+    //to adjust the /:restaurant_form_field
+    //receive the JSON response here from restaurant.js route
+    // ie. Hide the div 
+  });
+
+});
