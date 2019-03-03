955361b2a962ea91f3059e134ab84b1a52200704
Tristan <tristanberezowski@gmail.com>
2019-03-03 04:54:47 +0000

    order confirmation header and start of order_script

--- public/scripts/order_script.js
+++ public/scripts/order_script.js
@@ -1,3 +1,25 @@
 $(() => {
-  
-})
+  function createOrderElement(product, quantity) {
+    let element = `
+    <div class="d-flex justify-content-start order-item">
+        <div class="row">
+          <div class="col-12 col-md-2"><img class="order-item-image" src="${product.img}" /></div>
+          <div class="col-12 col-md-6 ">
+            <p class="order-item-name">${product.name}</p>
+            <div>
+              <p class="order-item-description">
+              ${product.description}
+              </p>
+            </div>
+          </div>
+  <div class="col-12 col-md-2 "><p class="order-item-quantity">Quantity: ${quantity}</p></div>
+          <div class="col-12 col-md-2 "><p class="order-item-price">$${product.price * quantity}</p></div>
+        </div>
+      </div>
+    `
+    let $element = $(element);
+    return $element
+  }
+  // ----------------------
+  //$(".checkout-form").on("click", )
+});
