3f5780f9c4276f5459c79cbba8f5a195eb432f16
Tristan <tristanberezowski@gmail.com>
2019-03-03 22:51:56 +0000

    route-debugging complete

--- public/scripts/order_script.js
+++ public/scripts/order_script.js
@@ -1,32 +1,30 @@
-$(() => {
-  function createOrderElement(product, quantity) {
-    let element = `
-    <div class="d-flex justify-content-start order-item">
-        <div class="row">
-          <div class="col-12 col-md-2"><img class="order-item-image" src="${product.img}" /></div>
-          <div class="col-12 col-md-6 ">
-            <p class="order-item-name">${product.name}</p>
-            <div>
-              <p class="order-item-description">
-              ${product.description}
-              </p>
-            </div>
+function createOrderElement(product, quantity) {
+  let element = `
+  <div class="d-flex justify-content-start order-item">
+      <div class="row">
+        <div class="col-12 col-md-2"><img class="order-item-image" src="${product.img}" /></div>
+        <div class="col-12 col-md-6 ">
+          <p class="order-item-name">${product.name}</p>
+          <div>
+            <p class="order-item-description">
+            ${product.description}
+            </p>
           </div>
-  <div class="col-12 col-md-2 "><p class="order-item-quantity">Quantity: ${quantity}</p></div>
-          <div class="col-12 col-md-2 "><p class="order-item-price">$${product.price * quantity}</p></div>
         </div>
+<div class="col-12 col-md-2 "><p class="order-item-quantity">Quantity: ${quantity}</p></div>
+        <div class="col-12 col-md-2 "><p class="order-item-price">$${product.price * quantity}</p></div>
       </div>
-    `
-    let $element = $(element);
-    return $element
-  }
+    </div>
+  `
+  let $element = $(element);
+  return $element
+}
   // ----------------------
-
+$(() => {
   //submit user data and display order confirmed asynchronously
   $(".checkout-form").submit(function(event) {
     event.preventDefault();
     let guestInfo = $(".checkout-form").serialize() + `&order_id=${window.location.pathname.replace("/orders/","")}`;
-    console.log(guestInfo);
     $.post("/orders", guestInfo).done(() => {
       $("#order-confirmation").css("opacity","1")
       $(".checkout-form button").attr("disabled", true);
@@ -35,4 +33,8 @@ $(() => {
       console.error("error posting in front end");
     })
   });
+
+  $.get("/api/order").done((items) => {
+    console.log(items);
+  })
 });
